4a38a8a5c7dfae312f735d6310439f78e9d1fe2d
Prumix <69EgadeTt>
2021-03-25 00:15:15 +0200

    Lesson5 Home Work

--- src/main/java/Lesson1/Main.java
+++ src/main/java/Lesson1/Main.java
@@ -2,6 +2,23 @@ package Lesson1;
 
 public class Main {
     public static void main(String[] args) {
+        Person[] person = new Person[5];
+        person[0] = new Person("Ivanov Ivan", "Engineer",
+                "ivivan@mailbox.com", "892312312", 30000, 30);
+        person[1] = new Person("Petrov Ivan", "Administrator",
+                "ivivan@mailbox.com", "892782312", 35000, 35);
+        person[2] = new Person("Ivanov Petr", "Cook",
+                "ivivan@mailbox.com", "898317912", 40000, 40);
+        person[3] = new Person("Smirnov Ivan", "Farmer",
+                "ivivan@mailbox.com", "892462738", 45000, 45);
+        person[4] = new Person("Smirnov Petr", "Maker-up",
+                "ivivan@mailbox.com", "892312372", 50000, 50);
 
+        for (int i = 0; i < person.length; i++) {
+            if (person[i].getAge() > 40){
+                System.out.println(person[i].toString());
+            }
+        }
     }
+
 }
